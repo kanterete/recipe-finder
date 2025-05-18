69b15a167adc95963592bfd47eb09165386da605
kanteretePC <kacperelsky2@gmail.com>
2025-05-18 15:56:44 +0200

    made hook

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,7 +1,5 @@
 import Card from "../components/Card"
-import { fetchRecipes } from "../api/fetchRecipes"
-import { useEffect, useState } from "react"
-import { Meals } from "../types/meals"
+import useFavorites from "../hooks/useFavorites"
 
 type CardsProps = {
     searchTerm: string
@@ -9,49 +7,10 @@ type CardsProps = {
 }
 
 const Cards = ({ searchTerm, searchFavorite }: CardsProps) => {
-    const [recipes, setRecipes] = useState<Meals[]>([])
-    const [favorites, setFavorites] = useState<string[]>(() => {
-        const storedFavorites = localStorage.getItem("favorites")
-        return storedFavorites ? JSON.parse(storedFavorites) : []
+    const { filteredRecipes, favorites, toggleFavorite } = useFavorites({
+        searchTerm,
+        searchFavorite,
     })
-
-    useEffect(() => {
-        localStorage.setItem("favorites", JSON.stringify(favorites))
-    }, [favorites])
-
-    useEffect(() => {
-        fetchRecipes().then((data) =>
-            setRecipes(
-                data.map((recipe: Meals) => ({
-                    ...recipe,
-                    favorite: false,
-                })),
-            ),
-        )
-    }, [])
-
-    const addFavorite = (prevFavorites: string[], mealId: string) =>
-        prevFavorites.includes(mealId)
-            ? prevFavorites
-            : [...prevFavorites, mealId]
-
-    const removeFavorite = (prevFavorites: string[], mealId: string) =>
-        prevFavorites.filter((id) => id !== mealId)
-
-    const toggleFavorite = (mealId: string) => {
-        setFavorites((prev) =>
-            prev.includes(mealId)
-                ? removeFavorite(prev, mealId)
-                : addFavorite(prev, mealId),
-        )
-    }
-
-    const filteredRecipes = recipes.filter(
-        (recipe) =>
-            recipe.name.toLowerCase().includes(searchTerm.toLowerCase()) &&
-            (!searchFavorite || favorites.includes(recipe.id)),
-    )
-
     return (
         <>
             <div>
---
+++ src/hooks/useFavorites.ts
@@ -0,0 +1,58 @@
+import { fetchRecipes } from "../api/fetchRecipes"
+import { useEffect, useState } from "react"
+import { Meals } from "../types/meals"
+
+type useFavoritesProps = {
+    searchTerm: string
+    searchFavorite: boolean
+}
+
+export default function useFavorites({
+    searchTerm,
+    searchFavorite,
+}: useFavoritesProps) {
+    const [recipes, setRecipes] = useState<Meals[]>([])
+    const [favorites, setFavorites] = useState<string[]>(() => {
+        const storedFavorites = localStorage.getItem("favorites")
+        return storedFavorites ? JSON.parse(storedFavorites) : []
+    })
+
+    useEffect(() => {
+        localStorage.setItem("favorites", JSON.stringify(favorites))
+    }, [favorites])
+
+    useEffect(() => {
+        fetchRecipes().then((data) =>
+            setRecipes(
+                data.map((recipe: Meals) => ({
+                    ...recipe,
+                    favorite: false,
+                })),
+            ),
+        )
+    }, [])
+
+    const addFavorite = (prevFavorites: string[], mealId: string) =>
+        prevFavorites.includes(mealId)
+            ? prevFavorites
+            : [...prevFavorites, mealId]
+
+    const removeFavorite = (prevFavorites: string[], mealId: string) =>
+        prevFavorites.filter((id) => id !== mealId)
+
+    const toggleFavorite = (mealId: string) => {
+        setFavorites((prev) =>
+            prev.includes(mealId)
+                ? removeFavorite(prev, mealId)
+                : addFavorite(prev, mealId),
+        )
+    }
+
+    const filteredRecipes = recipes.filter(
+        (recipe) =>
+            recipe.name.toLowerCase().includes(searchTerm.toLowerCase()) &&
+            (!searchFavorite || favorites.includes(recipe.id)),
+    )
+
+    return { filteredRecipes, favorites, toggleFavorite }
+}
